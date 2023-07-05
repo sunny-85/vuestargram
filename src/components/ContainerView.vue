476b4dc8efc16ef4a31faaadad1aa3cc383bfaac
sunny85 <sunhehrm@gmail.com>
2023-07-06 07:34:04 +0900

    뷰스타그램

---
+++ src/components/ContainerView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        컨테이너에요.
+        <Post />
+        <Post />
+        <Post />
+    </div>
+</template>
+
+<script>
+import Post from './PostView.vue';
+
+export default {
+    name : 'ContainerView',
+    components : {
+        Post,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
